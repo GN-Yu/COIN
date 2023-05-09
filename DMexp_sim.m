9e87ab3d1a0333e6a679036a28cfd442c0bffd9b
guoning-yu <guoning-yu@users.noreply.github.com>
2023-05-08 21:59:37 -0400

    Update DMexp_sim.m

--- DMexp_sim.m
+++ DMexp_sim.m
@@ -1,8 +1,21 @@
+% Define the length of the vector
+vectorLength = 192;  % Modify this value as per your desired length
+
+% Generate random integers between 1 and 3
+randomIntegers = rand([1, 3], 1, vectorLength);
+
+% Convert the random integers to the corresponding values: 0, -1, 1
+vector = randomIntegers - 2;
+
+% Display the generated vector
+% disp(vector);
+
+
 obj_DM = COIN;
 
-obj_DM.perturbations = [zeros(1,24) ones(1,60) zeros(1,24) -ones(1,60) zeros(1,24) NaN(1,192) zeros(1,24) -ones(1,60) zeros(1,24) ones(1,60) zeros(1,24) NaN(1,192)];
+obj_DM.perturbations = [zeros(1,24) ones(1,60) zeros(1,24) -ones(1,60) zeros(1,24) vector zeros(1,24) -ones(1,60) zeros(1,24) ones(1,60) zeros(1,24) vector];
 
-obj_DM.runs = 10;
+obj_DM.runs = 2;
 obj_DM.max_cores = feature('numcores');
 
 obj_DM.infer_bias = true;
